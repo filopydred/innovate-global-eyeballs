a75c5b19da320657b534ba7b65e2b38617438eca
Filopydred <143886295+filopydred@users.noreply.github.com>
2023-11-24 01:59:15 -0500

    Create code_2.py

---
+++ code_2.py
@@ -0,0 +1,26 @@
+
+def generate_random_data():
+    for item in data:
+        print(f"Random Number: {item}")
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+
+
+    data = generate_random_data()
+def main():
+    main()
+
+import random
+if __name__ == "__main__":
+import random
+    data = generate_random_data()
+    main()
+        print(f"Random Number: {item}")
+def main():
+    for item in data:
+
+if __name__ == "__main__":
+
+    data = [random.randint(1, 100) for _ in range(10)]
+def generate_random_data():
+    return data
